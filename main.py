812259756c22e9fef837690bd0b1dd42b0eb4801
blokz <thebloxpl@gmail.com>
2024-10-26 23:03:24 +0200

    add parse

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website , split_dom_content , clean_body_content , extract_body_content
 st.title("AI web scraper")
 url = st.text_input("Enter a website Url")
 
 if st.button("Scrape site"):
     result = scrape_website(url)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+    st.session_state.dom_conent = cleaned_content
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content , height=300)
+    
+    if "dom_content" in st.session_state:
+        parse_description = st.text_area("Descripe")
+
+        if st.button("parse"):
+            if parse_description:
+                st.write("parsing")
+
+                dom_chunks = split_dom_content(st.session_state.dom_content)
+                
